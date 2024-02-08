b6fd41ebd5972c57b373537f7b8559f5f71f8246
ddoonsim <2sujg97@naver.com>
2024-02-08 14:13:54 +0900

    jwt 회원가입 및 로그인 세팅 완료

--- src/test/java/org/choongang/member/MemberTest.java
+++ src/test/java/org/choongang/member/MemberTest.java
@@ -5,11 +5,12 @@ import org.choongang.member.service.MemberJoinService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 @Transactional
-//@TestPropertySource(properties = "spring.profiles.active=test")
+@TestPropertySource(properties = "spring.profiles.active=test")
 public class MemberTest {
 
     @Autowired

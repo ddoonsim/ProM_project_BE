070ab0bcf9f4c2160898748a2475791d165899a0
ddoonsim <2sujg97@naver.com>
2024-02-15 15:55:15 +0900

    이메일 전송 작업 중,,

---
+++ src/test/java/org/choongang/email/EmailApiTest.java
@@ -0,0 +1,37 @@
+package org.choongang.email;
+
+import jakarta.servlet.http.HttpSession;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class EmailApiTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @DisplayName("이메일 인증 코드 발급 및 검증 테스트")
+    void sendVerifyEmailTest() throws Exception {
+        /* 인증 코드 발급 테스트 S */
+        HttpSession session = mockMvc.perform(get("/api/email/verify?email=2sujg97@naver.com[교체부분]"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andReturn().getRequest().getSession();
+        Integer authNum = (Integer)session.getAttribute("EmailAuthNum");
+        /* 인증 코드 발급 테스트 E */
+
+        /* 인증 코드 검증 테스트 S */
+        mockMvc.perform(get("/api/email/auth_check?authNum=" + authNum.intValue()))
+                .andDo(print());
+        /* 인증 코드 검증 테스트 E */
+    }
+}

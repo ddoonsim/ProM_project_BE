edd9d35158dbc6db6fc069c1ddf1776a7da3e047
easy-0 <lc_dyna@naver.com>
2024-02-22 18:29:22 +0900

    주석추가

--- src/main/java/org/choongang/chatting/services/ChatRoomSaveService.java
+++ src/main/java/org/choongang/chatting/services/ChatRoomSaveService.java
@@ -14,6 +14,11 @@ public class ChatRoomSaveService {
     private final ChatRoomRepository repository;
     private final ProjectRepository projectRepository;
 
+    /**
+     * 초기 테스트용 채팅방 생성 양식 (삭제 예정)
+     * @param form
+     * @return
+     */
     public ChatRoom save(ChatRoomForm form) {
         Long roomNo = form.roomNo();
         ChatRoom room = null;
@@ -31,6 +36,13 @@ public class ChatRoomSaveService {
         return room;
     }
 
+    /**
+     * 채팅방 정보 수정시 사용
+     * @param roomNm
+     * @param projectSeq
+     * @param roomNo
+     * @return
+     */
     public ChatRoom save(String roomNm, Long projectSeq, Long roomNo) {
         ChatRoom room = null;
 
@@ -50,9 +62,21 @@ public class ChatRoomSaveService {
         return room;
     }
 
+    /**
+     * 자유 채팅방 생성
+     * @param roomNm
+     * @return
+     */
     public ChatRoom save(String roomNm) {
         return save(roomNm, null, null);
     }
+
+    /**
+     * 프로젝트 채팅방 생성
+     * @param roomNm
+     * @param projectSeq
+     * @return
+     */
     public ChatRoom save(String roomNm, Long projectSeq) {
         return save(roomNm, projectSeq, null);
     }
